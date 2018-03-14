1b2244ce009d8f50ea8e42eaaf5c9f120958bad8
Dunya <dunya.mohamed.2015@salleurl.edu>
2018-03-14 17:00:09 +0100

    Versio 0.1.0 - amb gitignore

---
+++ app/src/main/java/guerrero/com/app1/MainFeed.java
@@ -0,0 +1,16 @@
+package guerrero.com.app1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Guillem on 27/12/2017.
+ */
+
+public class MainFeed extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_feed);
+    }
+}
